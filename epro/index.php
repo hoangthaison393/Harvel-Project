1d38a4c6715a5d710f172e90acd45f4cb005d6f3
hoangthaison393 <81455267+hoangthaison393@users.noreply.github.com>
2021-04-05 15:56:55 +0700

    Add files via upload

---
+++ epro/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Web đồ điện gia dụng</title>
+</head>
+<body>
+    <div class="wrapper" id="">
+        <div class="header">
+            <h1>A</h1>
+        </div>
+        <div class="menu">
+            <h1>B</h1>     
+        </div>
+        <div class="main">
+            <h1>C</h1>
+        </div>
+        <div class="footer">
+            <h1>D</h1>
+        </div>
+    </div>
+</body>
+</html>
+
